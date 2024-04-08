722f670d595c7004cd1da32fddc050c239bc1d21
yeeun0118s <yeeun0118s@naver.com>
2024-04-08 16:38:58 +0900

    로그인&로그아웃

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,10 +1,16 @@
 package shop.mtcoding.blog.user;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
+    private final UserRepository userRepository;
+    private final HttpSession session;
 
     @GetMapping("/join-form")
     public String joinForm() {
@@ -16,6 +22,13 @@ public class UserController {
         return "user/login-form";
     }
 
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO reqDTO){
+        User sessionUser = userRepository.findByUsernameAndPassword(reqDTO.getUsername(), reqDTO.getPassword());
+        session.setAttribute("sessionUser", sessionUser);
+        return "redirect:/";
+    }
+
     @GetMapping("/user/update-form")
     public String updateForm() {
         return "user/update-form";
@@ -23,6 +36,7 @@ public class UserController {
 
     @GetMapping("/logout")
     public String logout() {
+        session.invalidate();
         return "redirect:/";
     }
 }
